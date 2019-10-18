5d3db93c2efb044ef62f34468a117ea27c271a4f
marx <marx@17track.cn>
2019-10-18 09:46:50 +0800

    修改:dep的调试

--- src/js/dep.js
+++ src/js/dep.js
@@ -1,15 +1,22 @@
+import { remove } from './util/index';
+
 export function Dep() {
     this.subs = [];
+    this.temp={txt:null};
 }
 
 Dep.prototype = {
     addSub: function (sub) {
         this.subs.push(sub);
     },
+    removeSub(sub) {
+        remove(this.subs, sub)
+    },
     notify: function () {
         this.subs.forEach(function (sub) {
-            sub.update();
-        })
+            sub.update(this.temp);
+        }.bind(this))
+        this.temp.txt=null;
     }
 }
 
