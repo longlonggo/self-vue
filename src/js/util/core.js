89d8fc5304d3421d85183affb5e2c2fe004612a7
marx <marx@17track.cn>
2019-10-18 09:47:28 +0800

    修改:util 拆成文件夹

---
+++ src/js/util/core.js
@@ -0,0 +1,36 @@
+import { isSpecificSymbol } from './tool';
+
+export function parsePath(path) {
+    path = path.replace(/\s/g, '');
+    if (isSpecificSymbol(path)) {
+        return console.error('调用路径的：' + path + '中含有特殊符号');
+    }
+
+    path = path.replace(/^\[|\]/g, '');
+    path = path.replace(/\[/g, '.');
+    var segments = path.split('.');
+
+    return segments;
+}
+
+
+export function getData(obj, segments) {
+    for (let i = 0; i < segments.length; i++) {
+        if (!obj) return;
+        obj = obj[segments[i]];
+    }
+    return obj;
+}
+
+export function getVmVal(vm, expr) {
+    return getData(vm.$data, parsePath(expr));
+}
+
+export function setVmVal(vm, expr, value) {
+    var data = vm.$data;
+    var segments = parsePath(expr);
+    for (var index = 0, len = segments.length - 1; index < len; index++) {
+        data = data[segments[index]];
+    }
+    data[segments[index]]=value;
+}
---
+++ src/js/util/tool.js
@@ -0,0 +1,48 @@
+export function isObject(obj) {
+    return obj !== null && typeof obj === 'object'
+};
+
+export function isElementNode(node) {
+    return node.nodeType === 1;
+};
+
+export function isTextNode(node) {
+    return node.nodeType === 3;
+};
+
+export function isCommentNode(node) {
+    return node.nodeType === 8;
+};
+
+export function isDirective(attr) {
+    return /^v-/.test(attr);
+};
+
+export function isEventDirective(dir) {
+    return /^on:/.test(dir)
+};
+
+export function isOnDirective(attr) {
+    return /^@/.test(attr);
+};
+export function isBindDirective(attr) {
+    return /^:/.test(attr);
+};
+
+export function isSpecificSymbol(str) {
+    return /[^\w.\[\]$]/.test(str);
+};
+
+export function remove(arr, item) {
+    if (arr.length) {
+        var index = arr.indexOf(item)
+        if (index > -1) {
+            return arr.splice(index, 1);
+        }
+    }
+};
+
+
+export function getUUID(index) {
+    return Number(Math.random().toString().substr(3, 5) + index + + Date.now()).toString(36);
+};
